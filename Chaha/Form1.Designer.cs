210662fb25a85364a7b6cac32affb61f41671cea
Андрей Соловов <solovov2003@yandex.ru>
2023-06-14 14:57:06 +0300

    Добавьте файлы проекта.

---
+++ Chaha/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Chaha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Запчасти Iphone 14";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(262, 425);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 156);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(262, 244);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(3, 403);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoEllipsis = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(-1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(262, 153);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(271, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(262, 425);
+            this.panel2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoEllipsis = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(-1, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(262, 153);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(3, 403);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "label5";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox2.Location = new System.Drawing.Point(-1, 156);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(262, 244);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.pictureBox3);
+            this.panel3.Location = new System.Drawing.Point(539, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(262, 425);
+            this.panel3.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoEllipsis = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(-1, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(262, 153);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(3, 403);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "label7";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox3.Location = new System.Drawing.Point(-1, 156);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(262, 244);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.label8);
+            this.panel4.Controls.Add(this.label9);
+            this.panel4.Controls.Add(this.pictureBox4);
+            this.panel4.Location = new System.Drawing.Point(807, 1);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(262, 425);
+            this.panel4.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.AutoEllipsis = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(-1, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(262, 153);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(3, 403);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 20);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "label9";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox4.Location = new System.Drawing.Point(-1, 156);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(262, 244);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 1;
+            this.pictureBox4.TabStop = false;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.panel1);
+            this.panel5.Controls.Add(this.panel4);
+            this.panel5.Controls.Add(this.panel2);
+            this.panel5.Controls.Add(this.panel3);
+            this.panel5.Location = new System.Drawing.Point(12, 37);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(1073, 430);
+            this.panel5.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1147, 500);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Panel panel5;
+    }
+}
+
---
+++ Chaha/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+namespace Chaha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Panel panel = GeneratePanel();
+            panel.Location = new Point(panel5.Location.X, panel5.Location.Y + panel5.Size.Height);
+            this.Controls.Add(panel);
+
+            HtmlWeb htmlWeb = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument document = htmlWeb.Load("https://woap.ru/zapchasti-dlya-iphone/zapchasti-iphone-14-pro-max");
+
+            HtmlNodeCollection iphone14_Nazvanie_Nodes = document.DocumentNode.SelectNodes("//div[@class='field-item odd']");
+            HtmlNodeCollection iphone14_Img_Nodes = document.DocumentNode.SelectNodes("//div[@class='field field-name-uc-product-image field-type-image field-label-hidden']");
+            HtmlNodeCollection iphone14_Cena_Nodes = document.DocumentNode.SelectNodes("//span[@class='uc-price']");
+
+            List<string> iphone14_Nazvanie = new List<string>();
+            List<string> iphone14_Img = new List<string>();
+            List<string> iphone14_Cena = new List<string>();
+
+            foreach(var item in iphone14_Nazvanie_Nodes)
+            {
+                iphone14_Nazvanie.Add(item.ChildNodes[0].InnerText);
+            }
+
+            foreach(var item in iphone14_Img_Nodes)
+            {
+                iphone14_Img.Add(item.ChildNodes.First().ChildNodes.First().Attributes["src"].Value);
+            }
+
+            foreach (var item in iphone14_Cena_Nodes)
+            {
+                iphone14_Cena.Add(item.InnerText);
+            }
+
+            label2.Text = iphone14_Nazvanie[0];
+            pictureBox1.Load(iphone14_Img[0]);
+            label3.Text = iphone14_Cena[0];
+        }
+
+        public Panel GeneratePanel()
+        {
+            Panel main = new Panel();
+            //main.BorderStyle = BorderStyle.FixedSingle;
+            main.Size = panel5.Size;
+
+            Panel panel1 = new Panel();
+            Panel panel2 = new Panel();
+            Panel panel3 = new Panel();
+            Panel panel4 = new Panel();
+
+            PictureBox picture1 = new PictureBox();
+            PictureBox picture2 = new PictureBox();
+            PictureBox picture3 = new PictureBox();
+            PictureBox picture4 = new PictureBox();
+
+            Label label1 = new Label();
+            Label label2 = new Label();
+            Label label3 = new Label();
+            Label label4 = new Label();
+
+            Label clabel1 = new Label();
+            Label clabel2 = new Label();
+            Label clabel3 = new Label();
+            Label clabel4 = new Label();
+
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(picture1);
+            panel1.Controls.Add(clabel1);
+
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(picture2);
+            panel2.Controls.Add(clabel2);
+
+            panel3.Controls.Add(label3);
+            panel3.Controls.Add(picture3);
+            panel3.Controls.Add(clabel3);
+
+            panel4.Controls.Add(label4);
+            panel4.Controls.Add(picture4);
+            panel4.Controls.Add(clabel4);
+
+            panel1.Size = this.panel1.Size;
+            panel2.Size = this.panel2.Size;
+            panel3.Size = this.panel3.Size;
+            panel4.Size = this.panel4.Size;
+
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel3.BorderStyle = BorderStyle.FixedSingle;
+            panel4.BorderStyle = BorderStyle.FixedSingle;
+
+            panel1.Margin = new Padding(3);
+            panel2.Margin = new Padding(3);
+            panel3.Margin = new Padding(3);
+            panel4.Margin = new Padding(3);
+
+            panel2.Location = new Point(panel1.Location.X + panel1.Size.Width + 6, panel1.Location.Y);
+            panel3.Location = new Point(panel2.Location.X + panel2.Size.Width + 6, panel2.Location.Y);
+            panel4.Location = new Point(panel3.Location.X + panel3.Size.Width + 6, panel3.Location.Y);
+
+            picture1.Size = pictureBox1.Size; 
+            picture2.Size = pictureBox2.Size;
+            picture3.Size = pictureBox3.Size;
+            picture4.Size = pictureBox4.Size;
+
+            label1.Size = this.label2.Size;
+            label2.Size = this.label4.Size;
+            label3.Size = this.label6.Size;
+            label4.Size = this.label8.Size;
+
+            clabel1.Size = this.label3.Size;
+            clabel2.Size = this.label5.Size;
+            clabel3.Size = this.label7.Size;
+            clabel4.Size = this.label9.Size;
+
+            main.Controls.Add(panel1);
+            main.Controls.Add(panel2);
+            main.Controls.Add(panel3);
+            main.Controls.Add(panel4);
+
+            return main;
+        }
+    }
+}
